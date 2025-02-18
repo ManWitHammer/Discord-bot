321ba5ed6625fd69a8cd37722fa10020a5e2ac37
Nik <152688371+ManWitHammer@users.noreply.github.com>
2025-02-18 20:55:44 +0300

    temporarily delete notifications

--- index.js
+++ index.js
@@ -38,118 +38,9 @@ const client = new Client({
   ],
 })
 
-const YOUTUBE_API_KEY = process.env.GOOGLE_API_KEY
-
-// Fetch the latest video from a YouTube channel by channel ID
-async function getLatestYouTubeVideo(channelId) {
-  try {
-    const response = await axios.get('https://www.googleapis.com/youtube/v3/search', {
-      params: {
-        part: 'snippet',
-        channelId: channelId,
-        order: 'date',
-        maxResults: 1,
-        type: 'video',
-        key: YOUTUBE_API_KEY
-      }
-    });
-    
-    const video = response.data.items[0];
-    if (video) {
-      return {
-        title: video.snippet.title,
-        videoId: video.id.videoId,
-        thumbnail: video.snippet.thumbnails.high.url
-      };
-    }
-    return null;
-  } catch (error) {
-    console.error('Error fetching latest YouTube video:', error);
-    return null;
-  }
-}
-
-// Function to check YouTube channel and notify for a new video
-async function checkYouTubeVideos() {
-  const subscriptions = await Subscription.find();
-
-  for (const sub of subscriptions) {
-    if (!sub.youtubeChannelId) continue;
-
-    const latestVideo = await getLatestYouTubeVideo(sub.youtubeChannelId);
-    if (latestVideo && latestVideo.videoId !== sub.lastVideoId) {
-      sub.lastVideoId = latestVideo.videoId;
-      await sub.save();
-
-      const channel = client.channels.cache.get(sub.discordChannelId);
-      if (channel) {
-        const embed = new EmbedBuilder()
-          .setTitle(`Новое видео на канале ${sub.youtubeChannelName}!`)
-          .setDescription(latestVideo.title)
-          .setURL(`https://www.youtube.com/watch?v=${latestVideo.videoId}`)
-          .setImage(latestVideo.thumbnail)
-          .setColor('Red');
-        channel.send({ embeds: [embed] });
-      }
-    }
-  }
-}
-
-const TWITCH_CLIENT_ID = process.env.TWITCH_CLIENT_ID;
-const TWITCH_CLIENT_SECRET = process.env.TWITCH_CLIENT_SECRET;
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v10');
 let token = process.env.TOKEN;
-
-async function getTwitchAccessToken() {
-  const response = await axios.post('https://id.twitch.tv/oauth2/token', null, {
-      params: {
-          client_id: TWITCH_CLIENT_ID,
-          client_secret: TWITCH_CLIENT_SECRET,
-          grant_type: 'client_credentials'
-      }
-  });
-  oauthToken = response.data.access_token;
-  return response.data.access_token;
-}
-
-async function getStreamInfo(twitchUsername) {
-  const response = await axios.get('https://api.twitch.tv/helix/streams', {
-      params: { user_login: twitchUsername },
-      headers: {
-          'Client-ID': process.env.TWITCH_CLIENT_ID,
-          'Authorization': `Bearer ${await getTwitchAccessToken()}`
-      }
-  });
-  return response.data.data[0] || null;
-}
-
-async function checkStreams() {
-  const subscriptions = await Subscription.find();
-
-  for (const sub of subscriptions) {
-      // Проверка, есть ли у документа имя пользователя Twitch
-      if (!sub.twitchUsername) continue;
-
-      const streamInfo = await getStreamInfo(sub.twitchUsername);
-      if (streamInfo && streamInfo.id !== sub.lastStreamId) {
-          sub.lastStreamId = streamInfo.id;
-          await sub.save();
-
-          const channel = client.channels.cache.get(sub.discordChannelId);
-          if (channel) {
-              const embed = new EmbedBuilder()
-                  .setTitle(`${streamInfo.user_name} Сейчас в сети!`)
-                  .setDescription(`В данный момент играет в ${streamInfo.game_name}`)
-                  .addFields({ name: `${streamInfo.title}`, value: `[Вот ссылка на канал](https://twitch.tv/${streamInfo.user_login})` })
-                  .setURL(`https://www.twitch.tv/${streamInfo.user_name}`)
-                  .setImage(streamInfo.thumbnail_url.replace('{width}x{height}', '1280x700'))
-                  .setColor('Purple');
-              channel.send({ embeds: [embed] });
-          }
-      }
-  }
-}
 
 module.exports = client;
 client.commands = new Collection();
@@ -202,10 +93,8 @@ mongoose.connect(process.env.URI_MONGO)
 const rest = new REST({ version: '10' }).setToken(token);
 client.on("ready", async () => {
   try {
-    setInterval(checkStreams, 5 * 60000);
-    setInterval(checkYouTubeVideos, 5 * 60000);
     client.user.setPresence({
-      activities: [{ name: 'на вас! (=^-ω-^=)', type: ActivityType.Watching }],
+      activities: [{ name: 'в окно (=^-ω-^=)', type: ActivityType.Watching }],
       status: 'idle',
     });
     await rest.put(
